fef4287b0f130f23cfc4b0a5c535411af3413580
SashaWindows <nikonorovsasha1@gmail.com>
2026-01-16 13:15:03 +0300

    Реализовал большинство критически важных функций в bst_tree (Кроме remove)

--- src/bst_tree.h
+++ src/bst_tree.h
@@ -1,31 +1,139 @@
 #ifndef BST_TREE_H
 #define BST_TREE_H
 
-template<typename T>
-class bst_tree {
-	struct tree_node {
-		T val;
-		tree_node* right;
-		tree_node* left;
+namespace MyDataStructures {
 
-		tree_node(){}
-		tree_node(T val) : this->val(val) {}
-		tree_node(T val, tree_node* left, tree_node* right) : this->val(val), this->left(left), this->right(right){}
+	template<typename T>
+	class bst_tree {
+		struct tree_node {
+			T val;
+			tree_node* right;
+			tree_node* left;
+
+			tree_node() = delete;
+			tree_node(T val) : val(val), left(nullptr), right(nullptr) {}
+			tree_node(T val, tree_node* left, tree_node* right) : val(val), left(left), right(right) {}
+		};
+
+		tree_node* head_;
+		size_t size_;
+
+		void req_insert(T new_node, tree_node* temp) {
+			if (size_ == 0) {
+				head_ = new tree_node(new_node);
+				++size_;
+				return;
+			}
+
+			if (temp && new_node == temp->val) {
+				return;
+			}
+			else if (!temp->right && new_node > temp->val) {
+				temp->right = new tree_node(new_node);
+				++size_;
+			}
+			else if (!temp->left && new_node < temp->val) {
+				temp->left = new tree_node(new_node);
+				++size_;
+			}
+			else if (new_node > temp->val) {
+				req_insert(new_node, temp->right);
+			}
+			else {
+				req_insert(new_node, temp->left);
+			}
+		}
+
+		T* req_find(T val, tree_node* temp) {
+			if (!temp) return nullptr;
+
+			if (val > temp->val) {
+				return req_find(val, temp->right);
+			}
+			else if (val < temp->val) {
+				return req_find(val, temp->left);
+			}
+			else {
+				return &(temp->val);
+			}
+		}
+
+		//bool req_remove(T val, tree_node* temp, tree_node* par) {
+		//	if (!temp) return false;
+
+		//	if (val == temp->val) {
+		//		--size_;
+		//		if (par) {
+
+		//		}
+		//		else {
+		//			insert()
+		//		}
+		//		return true;
+		//	}
+		//	else if (val < temp->val) {
+		//		return req_remove(val, temp->left);
+		//	}
+		//	else {
+		//		return req_remove(val, temp->right);
+		//	}
+		//}
+
+		void req_clear(tree_node* temp) {
+			if (temp) {
+				if (temp->left) req_clear(temp->left);
+				if (temp->right) req_clear(temp->right);
+				delete temp;
+			}
+		}
+
+	public:
+		bst_tree() : size_(0), head_(nullptr) {}
+		bst_tree(const bst_tree& other) {}
+		bst_tree(bst_tree&& other) noexcept {}
+		void operator=(const bst_tree& right) {}
+		void operator=(bst_tree&& right) noexcept {}
+
+		void insert(T new_node) {
+			req_insert(new_node, head_);
+		}
+
+		T* find(T val){
+			return req_find(val, head_);
+		}
+
+		bool contains(T val){
+			return find(val) != nullptr;
+		}
+
+		//bool remove(T val){
+		//	return req_remove(val, head_, nullptr);
+		//}
+
+
+		bool empty() {
+			return size_ == 0;
+		}
+
+		size_t size() {
+			return size_;
+		}
+
+		void clear(){
+			req_clear(head_);
+			size_ = 0;
+			head_ = nullptr;
+		}
+
+		size_t height(){}
+
+		~bst_tree() {
+			clear();
+		}
 	};
 
-	tree_node* head_;
-	size_t size_;
+}
 
-
-public:
-	bst_tree(){}
-
-	~bst_tree(){}
-};
-
-
-
-
-
+namespace mds = MyDataStructures;
 
 #endif
--- src/test_bst_tree.cpp
+++ src/test_bst_tree.cpp
@@ -1,14 +1,31 @@
 #include <gtest/gtest.h>
-#include "../src/list.h"
+#include "../src/bst_tree.h"
 #include <utility>
 
 using namespace mds;
 
 
 TEST(Bst_tree, AllFunctions) {
-
-	int x;
-
-
+	bst_tree<int> tr;
+	tr.insert(1);
+	tr.insert(2);
+	tr.insert(0);
+	tr.insert(1);
+	tr.insert(2);
+	tr.insert(0);
+	EXPECT_EQ(tr.contains(0), true);
+	EXPECT_EQ(*tr.find(0), 0);
+	tr.insert(5);
+	tr.insert(7);
+	EXPECT_EQ(tr.contains(8), false);
+	EXPECT_EQ(tr.find(8), nullptr);
+	tr.insert(6);
+	tr.insert(-5);
+	tr.insert(-2);
+	EXPECT_EQ(tr.contains(2), true);
+	EXPECT_EQ(*tr.find(2), 2);
+	EXPECT_EQ(tr.size(), 8);
+	tr.clear();
+	EXPECT_EQ(tr.size(), 0);
 
 }
